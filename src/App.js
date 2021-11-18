e92c3f1faac84f2675f0a13ddfeceb8550e4a7b8
DSenen <davidsenen@hotmail.com>
2021-11-18 23:48:21 +0000

    Updating the page on the Netlify section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    alert(`Count: ${count}`);
+  });
+
+  const handleClick = () => {
+    setCount((prevCount) =>  prevCount + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and have some fun
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 }
 
-export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React and have some fun
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
