6c875209d8d6256fc0eb25e02de9f531c578fb79
dsenen <david.senenruiz@sainsburys.co.uk>
2023-05-15 15:18:27 +0100

    Updating test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  screen.debug()
   const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
 });
